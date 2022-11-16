dbcb7a359e1fc817edb67387af02e28b1d856fbb
michelleyu1 <michelle_yu@berkeley.edu>
2022-11-15 20:07:38 -0800

    main outline

---
+++ _build/jupyter_execute/main.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Decomposing Errors in Gridded Snow Water Equivalent Products in the Upper Colorado River Basin
+
+# In[1]:
+
+
+import pandas as pd
+from IPython.display import Image
+
+
+# ## Introduction
+
+# 
+
+# ### Annual Cycle of SWE
+
+# In[ ]:
+
+
+
+
+
+# ## Data
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## Study Area
+
+# In[ ]:
+
+
+
+
+
+# ## UA SWE Reconstruction
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## Sensitivity Analysis
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## Error Decomposition
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## Discussion & Conclusion
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# In[ ]:
+
+
+
+
+
+# ## References
+
+# In[ ]:
+
+
+
+
